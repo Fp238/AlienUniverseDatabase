8e731fe0a94a840b827cfa827d75cedb2529b120
Filip <filip.flicinski@cyfroweszkoly.pl>
2025-11-20 09:22:07 +0100

    Show film info

--- AlienUniverseDatabaseIII/obj/Debug/net9.0/AlienUniverseDatabaseIII.AssemblyInfo.cs
+++ AlienUniverseDatabaseIII/obj/Debug/net9.0/AlienUniverseDatabaseIII.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlienUniverseDatabaseIII")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ff6f080b861d0548ecf1ad3558dfe73334170c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlienUniverseDatabaseIII")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlienUniverseDatabaseIII")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
