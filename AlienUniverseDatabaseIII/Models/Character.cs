54ff1f844364087320e3ef8b31e6b7015e6e0654
Filip <filip.flicinski@cyfroweszkoly.pl>
2025-11-20 09:47:45 +0100

    add characters data

---
+++ AlienUniverseDatabaseIII/Models/Character.cs
@@ -0,0 +1,12 @@
+﻿namespace AlienUniverseDatabaseIII.Models;
+
+public class Character
+{
+    public string Name { get; set; } = string.Empty;
+    public string Films { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string Actor { get; set; } = string.Empty;
+    public string Species { get; set; } = string.Empty;
+    public string BirthYear { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty;
+}
